25f8dba233e30ee57b9d17f117c26b49ad01bbd5
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-10-25 15:53:37 +0100

    Add folder hashing functionality and sample text files
    Refactored file_hasher.py to include functions for scanning folders and hashing all files within a directory. Added two sample text files in the 'text' directory for testing the new folder hashing feature.

--- file_hasher.py
+++ file_hasher.py
@@ -1,14 +1,37 @@
 import hashlib
+import os 
+
+
+def scan_folder(path):
+     file_list = []
+     for root, dirs, files in os.walk(path):
+          for file in files:
+               file_path = os.path.join(root, file)
+               file_list.append(file_path)
+     return file_list
+
+
 def hash_file(path):
-    hash = hashlib.sha256()
+    h = hashlib.sha256()
+    with open(path, "rb") as f:
+        while chunk := f.read(65536):
+            h.update(chunk)
+    return h.hexdigest()  
 
-    while True:
-            with open(path, "rb") as f:
-                while chunk:= f.read(65536):
-                 if not chunk:
-                        break
-                hash.update(chunk)
-            return hash.digest()
 
-                
-print(hash_file("file.txt"))
+
+def hash_folder(path):
+    files = scan_folder(path)
+    for file in files:
+        try:
+            file_hash = hash_file(file)
+            print(f"{file}: {file_hash}")
+        except Exception as e:
+            print(f"error reading or generating file {e}")
+
+
+
+
+hash_folder("text")
+    
+
